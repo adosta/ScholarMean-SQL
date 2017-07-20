e827f6fbb97eb94304da7527f356bd83ca25786d
AlexisGRM <alex-gio2.0@hotmail.com>
2017-07-20 13:45:58 -0600

    add campos nuevos de alumno
    se agregaron nuevos campos a la tabla de alumnos

--- app/models/alumno.js
+++ app/models/alumno.js
@@ -25,6 +25,121 @@ module.exports=function(sequelize, DataTypes){
 	FechaNac: {
 	    type: Sequelize.DATE
 	},
+	NumHijos: {
+	    type: Sequelize.STRING
+	},
+	Sexo: {
+	    type: Sequelize.STRING
+	},
+	EstadoCivil: {
+	    type: Sequelize.STRING
+	},
+	PadreTutor: {
+	    type: Sequelize.STRING
+	},
+	RFC: {
+	    type: Sequelize.STRING
+	},
+	Curp: {
+	    type: Sequelize.STRING
+	},
+	GrupoEtnico: {
+	    type: Sequelize.STRING
+	},
+	TrabajoDeAlumno: {
+	    type: Sequelize.STRING
+	},
+	NumDependientes: {
+	    type: Sequelize.STRING
+	},
+	Municipio: {
+	    type: Sequelize.STRING
+	},
+	Estado: {
+	    type: Sequelize.STRING
+	},
+	Pais: {
+	    type: Sequelize.STRING
+	},
+	Extranjero: {
+	    type: Sequelize.STRING
+	},
+	PadresExtranjeros: {
+	    type: Sequelize.STRING
+	},
+	Peso: {
+	    type: Sequelize.STRING
+	},
+	Estatura: {
+	    type: Sequelize.STRING
+	},
+	TipoSangre: {
+	    type: Sequelize.STRING
+	},
+	AntecedMedicos: {
+	    type: Sequelize.STRING
+	},
+	OtroAnteced: {
+	    type: Sequelize.STRING
+	},
+	discapacidad: {
+	    type: Sequelize.STRING
+	},
+	NumSeguroSocial: {
+	    type: Sequelize.STRING
+	},
+	Calle: {
+	    type: Sequelize.STRING
+	},
+	Colonia: {
+	    type: Sequelize.STRING
+	},
+	Ciudad: {
+	    type: Sequelize.STRING
+	},
+	NumExterior: {
+	    type: Sequelize.STRING
+	},
+	CodPost: {
+	    type: Sequelize.STRING
+	},
+	EstadoDomi: {
+	    type: Sequelize.STRING
+	},
+	TelefonoCasa: {
+	    type: Sequelize.STRING
+	},
+	TelefonoCel: {
+	    type: Sequelize.STRING
+	},
+	Facebook: {
+	    type: Sequelize.STRING
+	},
+	Preparatoria: {
+	    type: Sequelize.STRING
+	},
+	Especialidad: {
+	    type: Sequelize.STRING
+	},
+	PromedioFinal: {
+	    type: Sequelize.STRING
+	},
+	Semestre: {
+	    type: Sequelize.STRING
+	},
+	TurnoParaEntrevist: {
+	    type: Sequelize.STRING
+	},
+	Medios: {
+	    type: Sequelize.STRING
+	},
+	NumInstitucionesConsidera: {
+	    type: Sequelize.STRING
+	},
+	JustificacionDeEleccion: {
+	    type: Sequelize.STRING
+	},
+
   });
 
 };
